783552e8eb54d332ddef6b70c82c37368cf9fef4
Alex Hanson <33587381+alhanson7210@users.noreply.github.com>
2021-04-21 17:25:04 -0700

    Update app.py
    Sample change from an individual contributor

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 def hello():
   return 'Foo Bar Hello world!'
 
+@app.route('/contributor')
+def contribution():
+  return 'Contributing to simple routes using Flask by @alhanson7210'
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
